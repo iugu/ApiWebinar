ff8dc67f17b4d4b54e04987385d63be05a0c906b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-28 19:01:56 +0800

    Delete db directory

--- db/migrate/20170309172659_add_customer_id_and_subscription_id_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddCustomerIdAndSubscriptionIdToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :customer_id, :string
-    add_column :users, :subscription_id, :string
-  end
-end
--- db/migrate/20170316163902_add_iugu_tokens_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddIuguTokensToUser < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :api_live_token, :string
-    add_column :users, :api_test_token, :string
-    add_column :users, :api_user_token, :string
-    add_column :users, :iugu_account_id, :string
-  end
-end
