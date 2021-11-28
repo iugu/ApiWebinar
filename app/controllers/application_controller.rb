b3a9d59ad140ac41d22a60984fb13861a4c2051e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-28 17:28:47 +0800

    Delete app directory

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  private 
-
-  def not_authenticated
-      redirect_to session_new_path, alert: "Please login first"
-  end
-end
